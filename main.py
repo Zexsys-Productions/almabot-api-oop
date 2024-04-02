002425337dc562a15d30bb6c2d18e6bcaacea468
Steven Silitonga <steven.matt.sil@gmail.com>
2024-04-02 10:50:15 +0700

    updated endpoints

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ async def root():
     return {"Zexsys API Version": "2.0.5"}
 
 # from synchronous scraper
-@app.get("/gpa/")
+@app.get("/gpa")
 async def read_credentials(username: str, password: str):
     gpa = await asyncScraper.async_get_gpa(username, password)
     return {"gpa": f"{gpa}"}
 
 # from synchronous scraper
-@app.get("/gradeinfo/")
+@app.get("/gradeinfo")
 async def read_credentials(username: str, password: str):
     return scraper.get_grade_info(username, password)
